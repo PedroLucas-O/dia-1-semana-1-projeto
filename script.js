cc9db192398c55b2fc5e2aa5b56a9e7e30f469d8
Pedro Lucas Oliveira <148586318+PedroLucas-O@users.noreply.github.com>
2025-04-10 10:35:04 -0300

    Add files via upload
    Script

---
+++ script.js
@@ -0,0 +1,6 @@
+const nome = "Pedro Lucas"
+const sobrenome = " Oliveira Frazão"
+const nomeCompleto = nome + sobrenome
+let idade = 12
+let cidade = "São Luís"
+console.log ("Olá, eu me chamo " + nomeCompleto + ", tenho " + idade + " anos de idade, eu moro em " + cidade + " e tenho aulas de programação.");
